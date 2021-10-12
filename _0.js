92ccbafc613dade865c488aba5f953d3125ea7ac
Diego Pias <diegopias.91@gmail.com>
2021-10-12 22:49:02 +0200

    Ho animato i piedini!

--- _0.js
+++ _0.js
@@ -1,1 +1,21 @@
-console.log("New file js")
+
+$(".myButton").click(function(){
+var $dx = $('#path132'), l = $dx.length, i = 0;
+function right() {
+  $dx.eq(i % l).fadeIn(1000, function() {
+    $dx.eq(i % l).fadeOut(1000, right);
+    i++;
+  })
+}
+right();
+
+var $sx = $('#path133'), l = $sx.length, i = 0;
+function left() {
+  $sx.eq(i % l).fadeIn(1000, function() {
+    $sx.eq(i % l).fadeOut(1000, left);
+    i++;
+  })
+};
+setTimeout(left,1000);
+
+});
